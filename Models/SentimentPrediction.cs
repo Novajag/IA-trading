7faa530802f7ec36e3fb85d3eed4ea6af0faefc3
unknown <nova2005jag@gmail.com>
2025-10-06 18:09:16 -0300

    Mi primer commit

---
+++ Models/SentimentPrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace ML_2025.Models;
+public class SentimentPrediction
+{
+    [ColumnName("PredictedLabel")]
+    public bool PredictedLabel { get; set; }
+    public float Probability { get; set; }
+    public float Score { get; set; }
+}
